1a252aaddfc67e504afefa3fbaa414e3b097fa5b
zhangxingyu <xy.zhang.work@foxmail.com>
2022-08-27 00:24:50 +0800

    sku和spu以及Minio(后台管理系统完结)

--- service/service-product/src/main/java/com/atguigu/gmall/product/ProductMainApplication.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/ProductMainApplication.java
@@ -1,16 +1,30 @@
 package com.atguigu.gmall.product;
 
+import com.atguigu.gmall.common.config.Swagger2Config;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author zhangxingyu
  * @create 2022/8/23  9:16
  * @description
+ * 默认只扫描 主程序所在的包和子包
+ * 主程序 com.atguigu.gmall.product
+ * 其他 com.atguigu.gmall.common.config
+ * 1.批量导入 @ComponentScan("com.atguigu.gmall.common.config")
+ * 2.批量导入 @SpringBootAppliaction(scanBasePackes = "com.atguigu.gmall")
+ * 3.精准导入 @Import(Swagger2Config.class) 需要什么导入什么
  */
+
+
+@Import(Swagger2Config.class)
 @SpringCloudApplication
 @MapperScan("com.atguigu.gmall.product.mapper")
+
 public class ProductMainApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductMainApplication.class,args);
--- service/service-product/src/main/java/com/atguigu/gmall/product/controller/CategoryController.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/controller/CategoryController.java
@@ -7,6 +7,8 @@ import com.atguigu.gmall.model.product.BaseCategory3;
 import com.atguigu.gmall.product.service.BaseCategory1Service;
 import com.atguigu.gmall.product.service.BaseCategory2Service;
 import com.atguigu.gmall.product.service.BaseCategory3Service;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +21,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/admin/product")
+@Api(tags = "三级分类")
 public class CategoryController {
 
 
@@ -34,6 +37,7 @@ public class CategoryController {
     /*
         获取所有的一级分类
      */
+    @ApiOperation(value = "获取一级分类")
     @GetMapping("/getCategory1")
     public Result getCategory1() {
         List<BaseCategory1> category1List = baseCategory1Service.list();
@@ -45,6 +49,7 @@ public class CategoryController {
        获取一级分类对应的二级分类
        /admin/product/getCategory2/2
      */
+    @ApiOperation(value = "获取一级分类对应的二级分类")
     @GetMapping("/getCategory2/{c1id}")
     public Result getCategory2(@PathVariable("c1id") Long c1id) {
         List<BaseCategory2> category2List = baseCategory2Service.getCategory1Child(c1id);
@@ -55,6 +60,7 @@ public class CategoryController {
         获取二级分类对应的三级分类
         /admin/product/getCategory3
      */
+    @ApiOperation(value = "获取二级分类对应的三级分类")
     @GetMapping("/getCategory3/{c2id}")
     public Result getCategory3(@PathVariable("c2id") Long c2id) {
         List<BaseCategory3> category3List = baseCategory3Service.getCateGory2Child(c2id);
